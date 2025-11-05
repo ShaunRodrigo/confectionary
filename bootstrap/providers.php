70a3fb388855917efa6f8966ada6cb0240ac9fd7
Shaun_R <161319179+ShaunRodrigo@users.noreply.github.com>
2025-11-05 23:20:31 +0100

    Move HTTPS scheme enforcement to AppServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,21 +1,5 @@
 <?php
 
-namespace App\Providers;
-
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
-
-class AppServiceProvider extends ServiceProvider
-{
-    public function register()
-    {
-        //
-    }
-
-    public function boot()
-    {
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
-    }
-}
+return [
+    App\Providers\AppServiceProvider::class,
+];
